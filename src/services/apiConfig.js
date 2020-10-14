1c9477c9d092897542974572456de48f663a7226
jeremyrrose <jeremy.r.rose@gmail.com>
2020-10-14 12:45:43 -0400

    deployed API for workshop

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,9 +1,11 @@
 import Axios from 'axios'
 export const JwtToken = () => localStorage.getItem('token') || null;
 
-const apiUrl = window.location.hostname === 'localhost' ?
-  'http://localhost:8000/' :
-  null // replace with deployed API URL
+// const apiUrl = window.location.hostname === 'localhost' ?
+//   'http://localhost:8000/' :
+//   null // replace with deployed API URL
+
+const apiUrl = 'http://record-collection-django.herokuapp.com/'
 
 const Api = Axios.create({
   baseURL: apiUrl,
